f4a1f812d0ba7c3b06d49acb46b006a0b2e8f375
TroinovAnatoly <158019793+TroinovAnatoly@users.noreply.github.com>
2026-04-03 17:53:16 +0700

    SUPER_MEGA_LAST_COMMIT

--- Front-end/src/pages/CartPage.js
+++ Front-end/src/pages/CartPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../app/store/hooks/reduxHooks";
+import "../styles/cart.css";
 
 import {
   fetchCart,
@@ -27,7 +28,7 @@ function CartPage() {
   if (!cart) return <p>Загрузка...</p>;
 
   return (
-    <div id="content">
+    <div id="content_cart">
       <h2>Корзина</h2>
 
       {(!cart.items || cart.items.length === 0) && (
@@ -35,13 +36,13 @@ function CartPage() {
       )}
 
       {cart.items?.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} class="item">
           <p>
             {item.game?.title || item.item?.title} —{" "}
             {item.game?.price || item.item?.price} ₽
           </p>
 
-          <button onClick={() => handleRemove(item.id)}>
+          <button onClick={() => handleRemove(item.id)} class="item_button">
             Удалить
           </button>
         </div>
@@ -49,7 +50,7 @@ function CartPage() {
 
       <h3>Итого: {cart?.total ?? 0} ₽</h3>
 
-      <button onClick={handleBuy}>
+      <button onClick={handleBuy} class="buy_cart_button">
         Купить
       </button>
     </div>
